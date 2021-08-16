c1e06369fb0005b641e83999fb8f4b25f999cc87
bonnett93 <bonnettjordan@gmail.com>
2021-08-15 22:04:10 -0500

    Remove a lot

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,6 @@
 # create the file ssh/config
-node default{
-  account { 'ubuntu':
-    ensure => present,
-  }
-  $ssh_user = 'ubuntu'
-  file{'~/.ssh/ssh_config'
+file{'~/.ssh/ssh_config'
   ensure  => present,
   content => 'PasswordAuthentication no
-              Host ubutu
-                HostName 34.138.233.200
-                User ubuntu
-                Port 22
-                IdentityFile ~/.ssh/holberton'
+              ~/.ssh/holberton'
   }
-}
