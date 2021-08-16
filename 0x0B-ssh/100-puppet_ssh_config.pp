abe0b5cbeb02e45268b82b458f1cf4e47d1d14dd
bonnett93 <bonnettjordan@gmail.com>
2021-08-15 22:06:13 -0500

    change path

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # create the file ssh/config
-file{'~/.ssh/ssh_config'
+file{'~/etc/ssh/ssh_config'
   ensure  => present,
   content => 'PasswordAuthentication no
               ~/.ssh/holberton'
-  }
+}
