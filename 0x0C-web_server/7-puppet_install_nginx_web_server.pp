60f724d2eb7d4b1636dda9eff318adbaf592fb64
bonnett93 <bonnettjordan@gmail.com>
2021-08-18 23:40:29 -0500

    remove node default

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,14 +1,12 @@
 # Install Nginx web server (w/ Puppet)
-node default {
-  package{'nginx':
-    ensure  => installed}
-  file{'/var/www/html/index.html':
-    ensure  => present,
-    content => 'Holberton School'}
-  suffix="server_name _;"
-  new_string_3="\n\tlocation /redirect_me{\n\t\treturn 301 https://www.linkedin.com/in/jordan-bonnett-gomez/;\n\t}"
-  exec{"sed -i "/${suffix}/a\\${new_string_3}" /etc/nginx/sites-available/default":
+package{'nginx':
+  ensure  => installed}
+file{'/var/www/html/index.html':
+  ensure  => present,
+  content => 'Holberton School'}
+suffix="server_name _;"
+new_string_3="\n\tlocation /redirect_me{\n\t\treturn 301 https://www.linkedin.com/in/jordan-bonnett-gomez/;\n\t}"
+exec{"sed -i "/${suffix}/a\\${new_string_3}" /etc/nginx/sites-available/default":
   path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']}
-  exec{"sudo service nginx restart":
+exec{"sudo service nginx restart":
   path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']}
-}
