23504f72cef0f7fb3b81e14f0cd05fb28f794875
bonnett93 <bonnettjordan@gmail.com>
2021-08-19 00:00:11 -0500

    Upload task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,12 +1,20 @@
 # Install Nginx web server (w/ Puppet)
-package{'nginx':
-  ensure  => installed}
-file{'/var/www/html/index.html':
+package {'nginx':
+  ensure  => installed
+}
+
+file {'/var/www/html/index.html':
   ensure  => present,
-  content => 'Holberton School'}
-suffix="server_name _;"
-new_string_3="\n\tlocation /redirect_me{\n\t\treturn 301 https://www.linkedin.com/in/jordan-bonnett-gomez/;\n\t}"
-exec{"sed -i "/${suffix}/a\\${new_string_3}" /etc/nginx/sites-available/default":
-  path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']}
-exec{"sudo service nginx restart":
-  path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']}
+  content => 'Holberton School'
+}
+
+file_line {'/etc/nginx/sites-available/default':
+  ensure => 'present',
+  after  => 'server_name _;',
+  line   => '\n\tlocation /redirect_me{\n\t\treturn 301 https://www.linkedin.com/in/jordan-bonnett-gomez/;\n\t}',
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
