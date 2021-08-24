9ccf518026ad4ea8934f5914789302151282c1c6
bonnett93 <bonnettjordan@gmail.com>
2021-08-24 13:26:37 -0500

    Upload task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,10 @@
 # Add a custom HTTP header with Puppet
 
 exec {'sudo apt-get -y update':
+	command => 'sudo apt-get -y update',
 	path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
 }
+->
 
 package {'instalar nginx':
 	ensure => present,
@@ -13,26 +15,28 @@ exec {'echo "Holberton School for the win!" >> /var/www/html/index.html':
 	path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
 }
 
-suffix='server_name _;'
-new_string_3='\n\tlocation /redirect_me{\n\t\treturn 301 https://www.linkedin.com/in/jordan-bonnett-gomez/;\n\t}'
-exec {'sed -i "/$suffix/a\\$new_string_3" /etc/nginx/sites-available/default':
+exec {'modify default nginx':
+	command => 'sudo sed -i "/server_name _;/a\\\n\tlocation /redirect_me{\n\t\treturn 301 https://www.google.com;\n\t}" /etc/nginx/sites-available/default',
 	path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
 }
 
-exec{'sudo echo "Ceci n'est pas une page" >> /var/www/html/error404.html':
+exec{'Add 404 page':
+	command => 'echo "Ceci n\'est pas une page" > /var/www/html/error404.html',
 	path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
 }
 
-new_string_4="\n\terror_page 404 /error404.html;"
-exec {'sed -i "/$suffix/a\\$new_string_4" /etc/nginx/sites-available/default':
+exec {'Addd 404 in default nginx':
+	command => 'sed -i "/server_name _;/a\\\n\terror_page 404 /error404.html;" /etc/nginx/sites-available/default',
 	path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
 }
 
-new_string_F0='\n\tadd_header X-Served-By $hostname;\n'
-exec{'sed -i "/$suffix/a\\$new_string_F0" /etc/nginx/sites-available/default':
-	path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
+file_line {'Add new header':
+	ensure => present,
+	path => '/etc/nginx/sites-available/default',
+	after => 'server_name _;',
+	line => '\n\tadd_header X-Served-By ${hostname};\n'
 }
 
-exec{'sudo service nginx restart':
-	path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
+exec {'restart nginx':
+	command => 'sudo service nginx restart'
 }
